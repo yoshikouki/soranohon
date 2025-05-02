a20eefd4f77ff72c3283e9ad5a4dcb41013b28d9
yoshikouki <yoshikouki@pepabo.com>
2025-05-02 23:55:55 +0900

    Update books

--- src/books/index.ts
+++ src/books/index.ts
@@ -1,8 +1,19 @@
-export const books = {
+export type Book = {
+  id: string;
+  title: string;
+  creator: string;
+  translator: string | undefined;
+  bibliographyRaw: string;
+  mdx: () => Promise<unknown>;
+};
+
+export const books: Record<string, Book> = {
   "59835_72466": {
     id: "59835_72466",
     title: "赤ずきん",
-    description: "赤ずきんの物語",
-    mdx: () => import("@/books/59835_72466.mdx"),
+    creator: "グリム Grimm",
+    translator: "矢崎源九郎",
+    bibliographyRaw: `底本：「グリム童話集（1）」偕成社文庫、偕成社\n1980（昭和55）年6月1刷\n2009（平成21）年6月49刷\n入力：sogo\n校正：チエコ\n2020年12月27日作成`,
+    mdx: () => import("./59835_72466.mdx"),
   },
 } as const;
