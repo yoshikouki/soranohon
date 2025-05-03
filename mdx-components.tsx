20e20b713fdbe3825aaa442450d72a228503245d
yoshikouki <yoshikouki@pepabo.com>
2025-05-03 16:03:05 +0900

    Revert "Beautiful line break"
    This reverts commit c48241ee9aa65f98f44d8ead65d7cc0ec293aaef.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,19 +1,7 @@
 import type { MDXComponents } from "mdx/types";
-import { JapaneseTextFormatter } from "@/features/books/JapaneseTextFormatter";
 
-/**
- * MDXコンポーネントのカスタマイズ
- * 日本語テキストの段落に自然な改行処理を適用します
- */
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    // 段落に日本語改行最適化を適用
-    p: ({ children }) => (
-      <p>
-        <JapaneseTextFormatter>{children}</JapaneseTextFormatter>
-      </p>
-    ),
-    // その他のコンポーネントは変更なし
     ...components,
   };
 }
