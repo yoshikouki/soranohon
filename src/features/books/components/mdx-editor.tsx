b266b4dea4529062f9f31bec50ceac2cdb71ae20
yoshikouki <yoshikouki@pepabo.com>
2025-05-09 21:47:16 +0900

    Show contents length

--- src/features/books/components/mdx-editor.tsx
+++ src/features/books/components/mdx-editor.tsx
@@ -8,6 +8,10 @@ interface MdxEditorProps {
   onChange?: (content: string) => void;
 }
 
+const removeTags = (content: string | undefined) => {
+  return content?.replace(/<rt>.*?<\/rt>/g, "").replace(/<.*?>/g, "") || "";
+};
+
 export const MdxEditor = ({ initialContent, onChange }: MdxEditorProps) => {
   const [content, setContent] = useState(initialContent);
 
@@ -21,15 +25,28 @@ export const MdxEditor = ({ initialContent, onChange }: MdxEditorProps) => {
     onChange?.(newContent);
   };
 
+  const contentWithoutTags = removeTags(content);
+
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="font-semibold text-xl">MDXコンテンツ編集</h2>
-      <Textarea
-        className="h-96 w-full resize-y font-mono"
-        value={content}
-        onChange={handleChange}
-        placeholder="MDXコンテンツを編集..."
-      />
+      <h2 className="font-semibold text-xl">MDXコンテンツ</h2>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <div>Contents:</div>
+          <div>{content?.length.toLocaleString() || "0"}</div>
+        </div>
+        <div>
+          <div>Contents without tags:</div>
+          <div>{contentWithoutTags.length.toLocaleString() || "0"}</div>
+        </div>
+        <div className="col-span-2">
+          <Textarea
+            className="h-96 w-full resize-y font-mono"
+            value={contentWithoutTags}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
